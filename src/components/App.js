856a35cbab55433b891b5147d497a61fce3bb0f7
Shubham Jain <shubham.rapariya2@gmail.com>
2023-12-13 13:22:59 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import Paragraph from "./Paragraph";
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Paragraph/>
+    </div>
   )
 }
 
---
+++ src/components/Paragraph.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Paragraph = () => {
+  return (
+    <p>
+        I am learning React. My life is getting better.
+    </p>
+  )
+}
+
+export default Paragraph
